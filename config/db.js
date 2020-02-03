a0163ad2505517f157a73b7680915ee042a7434f
David <xiaodi.david@gmail.com>
2020-02-03 23:36:18 +1000

    use morgan for logs

--- config/db.js
+++ config/db.js
@@ -20,15 +20,21 @@ if (env === 'dev') {
   }
 }
 
-// if (env === 'production') {
-//   MYSQL_CONF = {
-//     host: 'localhost',
-//     user: 'david',
-//     password: 'P@ssw0rd',
-//     port: '3306',
-//     database: 'myblog'
-//   }
-// }
+if (env === 'production') {
+  MYSQL_CONF = {
+    host: 'localhost',
+    user: 'david',
+    password: 'P@ssw0rd',
+    port: '3306',
+    database: 'myblog'
+  }
+
+  // redis
+  REDIS_CONF = {
+    port: 6379,
+    host: '127.0.0.1'
+  }
+}
 
 module.exports = {
   MYSQL_CONF,
